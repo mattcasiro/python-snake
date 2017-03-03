7bce700aa3930540a981002d693616c539d0f0da
JNotelddim <jaredmiddleton3.14@gmail.com>
2017-03-02 16:54:53 -0800

    added Food, Board, and Snake python class files

---
+++ app/Board.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import bottle, math, os
+
+# Battlesnake 2017 
+# Casiro, Chan, Middleton
+# Board Class file 
+
+class Board:
+    def __init__(self):
+
+
+
+    def __fn2__(self):
---
+++ app/Snake.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import bottle, math, os
+
+# Battlesnake 2017 
+# Casiro, Chan, Middleton
+# Snake Class file 
+
+class Snake:
+    def __init__(self):
+
+
+
+    def __fn2__(self):
