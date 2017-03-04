1ccf5b5c9936a5813e2f58f2dd8db0c4a6e3c2e1
JNotelddim <jaredmiddleton3.14@gmail.com>
2017-03-04 10:02:36 -0800

    updated snake init function

--- app/Snake.py
+++ app/Snake.py
@@ -5,5 +5,17 @@
 # Snake Class file 
 
 class Snake:
-    def __init__(self):
-        pass
+    def __init__(self, data):
+        #parse data array for snake indormation
+        self.coords = data['coords']
+        self.health = data['health']
+        self.id = data['id']
+        self.name = data['name']
+        self.taunt = data['taunt']
+        self.length = len(coords)
+
+        #assign left, right, up, and down variables
+        self.left = coords[0][0] - 1
+        self.right = coords[0][0] + 1
+        self.up = coords[0][1] - 1
+        self.down = coords[0][1] + 1
