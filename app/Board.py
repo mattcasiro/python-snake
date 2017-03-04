bef21b1591986b6568b0dc4cc59e0b1c6a17357a
JNotelddim <jaredmiddleton3.14@gmail.com>
2017-03-04 09:37:02 -0800

    fixed class files

--- app/Board.py
+++ app/Board.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-import bottle, math, os
 
 # Battlesnake 2017 
 # Casiro, Chan, Middleton
 # Board Class file 
 
 class Board:
-    def __init__(self):
+    def __init__(self, data):
+        self.game_id = data['game_id'];
+        
 
 
 
-    def __fn2__(self):
+    def start(self):
+        pass
--- app/Food.py
+++ app/Food.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import bottle, math, os
 
 # Battlesnake 2017 
 # Casiro, Chan, Middleton
@@ -7,7 +6,4 @@ import bottle, math, os
 
 class Food:
     def __init__(self):
-
-
-
-    def __fn2__(self):
+        pass
