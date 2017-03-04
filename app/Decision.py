561ab3f1ddc19b036d2db29411f48c555191a20b
JNotelddim <jaredmiddleton3.14@gmail.com>
2017-03-04 12:38:14 -0800

    select choice + syntax fixes

--- app/Decision.py
+++ app/Decision.py
@@ -1,3 +1,4 @@
+import random
 def decide(board):
     our_snake = board.our_snake
     
@@ -10,12 +11,14 @@ def decide(board):
     directions = ['left','right','up','down']
 
     for direction in directions:
-        if our_snake.moves[direction] in board.get_bad_squares:
+        if our_snake.moves[direction] in board.get_bad_squares():
             valid_moves[direction] = False
 
-    
+    for move in valid_moves:
+        if valid_moves[move]:
+            choice = move
 
     return {
-        'move': random.choice(valid_moves),
+        'move': choice,
         'taunt': 'For Noodle!'
     }
