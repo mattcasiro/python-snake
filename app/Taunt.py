c672521ce56591860756b72bc7357d6ec4e4b701
dalechan <dale.vincent.chan@gmail.com>
2017-03-04 13:46:32 -0800

    Create Taunt class

---
+++ app/Taunt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# Battlesnake 2017 
+# Casiro, Chan, Middleton
+# Board Class file
+
+from random import randint
+
+class Taunt:
+    def populate_array(self):
+        taunt_file = open('../resources/taunts.txt', 'r')
+        for line in taunt_file:
+            self.taunt_array.append(line)
+
+    def grab_taunt(self):
+        choice = randint(0,len(self.taunt_array) - 1)
+        return self.taunt_array[choice]
+
+    def __init__(self):
+        self.taunt_array = []
+        self.populate_array()
+        
