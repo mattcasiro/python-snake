b486a0809c8a89dd9db9163497de5d2f4c1dbf25
JNotelddim <jaredmiddleton3.14@gmail.com>
2017-03-04 15:45:28 -0800

    Target Food

--- app/Decision.py
+++ app/Decision.py
@@ -1,25 +1,54 @@
 import random
 def decide(board):
     our_snake = board.our_snake
-    
-    # bad_squares = getBadSquares()
+    nearest_food = board.nearest_foods[our_snake.id]
+
+    directions = ['left','right','up','down']
+
+
+
+    # DONT HIT A WALL OR A SNAKE
     valid_moves = { 'left': True,
                     'right': True,
                     'up': True,
                     'down': True }
-
-    directions = ['left','right','up','down']
-
     for direction in directions:
         # NOTE: call get_bad_squares() once if response time issue
         if our_snake.moves[direction] in board.get_bad_squares():
             valid_moves[direction] = False
 
+
+    # preferred moves (like targeting food)
+    preferred_moves = { 'left': False,
+                    'right': False,
+                    'up': False,
+                    'down': False }
+    # target food
+    if our_snake.head[0] < nearest_food[0]:
+        preferred_moves['right'] = True;
+    elif our_snake.head[0] > nearest_food[0]:
+        preferred_moves['left'] = True;
+
+    if our_snake.head[1] < nearest_food[1]:
+        preferred_moves['down'] = True;
+    if our_snake.head[1] > nearest_food[1]:
+        preferred_moves['up'] = True;
+    print preferred_moves
+    print valid_moves
+
+
+    #pick a move
     for move in valid_moves:
-        if valid_moves[move]:
+        if preferred_moves[move]:
             choice = move
+    if not valid_moves[choice]:
+        for move in valid_moves:
+            if valid_moves[move]:
+                choice = move
+
+    print choice
 
     return {
         'move': choice,
-        'taunt': 'For Noodle!'
+        'taunt': our_snake.id
     }
