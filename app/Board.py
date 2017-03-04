2e694c8a1f046566276183f26d283192531564b3
Matthew Casiro <mattcasiro@gmail.com>
2017-03-04 11:38:54 -0800

    Re-order methods for proper definition order

--- app/Board.py
+++ app/Board.py
@@ -5,19 +5,6 @@
 # Board Class file 
 
 class Board:
-    def __init__(self, data):
-        # Extract basics
-        self.game_id = data['game_id']
-        self.height = data['height']
-        self.width = data['width']
-        self.game_turn = data['turn']
-        self.our_id = data['you']
-
-        # Extract arrays
-        self.snakes, self.our_snake = get_live_snakes(data['snakes'])
-        self.dead_snakes = get_dead_snakes(data['dead_snakes'])
-        self.foods = data['food']
-
     def get_live_snakes(self, raw_snakes):
         snakes = []
         for raw_snake in raw_snakes:
@@ -34,3 +21,16 @@ class Board:
             dead_snakes.append(Snake(raw_snake))
 
         return dead_snakes
+    
+    def __init__(self, data):
+        # Extract basics
+        self.game_id = data['game_id']
+        self.height = data['height']
+        self.width = data['width']
+        self.game_turn = data['turn']
+        self.our_id = data['you']
+
+        # Extract arrays
+        self.snakes, self.our_snake = get_live_snakes(data['snakes'])
+        self.dead_snakes = get_dead_snakes(data['dead_snakes'])
+        self.foods = data['food']
