480e9b1c61714e81830870f4abf39063def950bf
JNotelddim <jaredmiddleton3.14@gmail.com>
2017-03-04 15:02:58 -0800

    associate snakes with nearest foods

--- app/Board.py
+++ app/Board.py
@@ -47,6 +47,33 @@ class Board:
 
         return bad
 
+    def associate_nearest_foods(self, snakes, our_snake, foods):
+        all_snakes = snakes + [our_snake]
+        # check for nulls
+        if not foods or not all_snakes:
+            print("foods or all_snakes null")
+            return
+
+        nearest_foods = {}
+        for snake in all_snakes:
+            #calculate nearest food for current snake:
+            i=0
+            nearest_index = 0
+            nearest_distance = 999999
+            for food in foods:
+                #calculate absolute val of differences between head and food x & y
+                distance = abs(snake.head[0] - food[0]) + abs(snake.head[1] - food[1])
+                #compare distance of current food to other foods
+                if distance < nearest_distance:
+                    nearest_distance = distance
+                    nearest_index = i
+                i += 1
+            #put current snake's nearest food in dictionary of snake:nearestfoods
+            nearest_foods[snake.id] = foods[nearest_index]
+        print nearest_foods
+        return nearest_foods
+
+
     def __init__(self, data):
         # Extract basics
         self.game_id = data['game_id']
@@ -59,3 +86,4 @@ class Board:
         self.snakes, self.our_snake = self.get_live_snakes(data['snakes'])
         self.dead_snakes = self.get_dead_snakes(data['dead_snakes'])
         self.foods = data['food']
+        self.nearest_foods = self.associate_nearest_foods(self.snakes, self.our_snake, self.foods)
