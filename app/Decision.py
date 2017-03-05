be8cad8178ef81c96ac15b37171c68b729d16957
dalechan <dale.vincent.chan@gmail.com>
2017-03-04 16:10:19 -0800

    Fix first_five method by removing self

--- app/Decision.py
+++ app/Decision.py
@@ -74,7 +74,7 @@ def decide(board):
         'taunt': our_snake.id
     }
 
-def first_five(self):
+def first_five():
     try:
         five_file = open('../resources/movefile.txt', 'w')
         our_snake = board.our_snake
