cf611325e682711a5453eb5ff48a6277da70b2f1
dalechan <dale.vincent.chan@gmail.com>
2017-03-04 16:13:09 -0800

    Fix first_five location in decesion file

--- app/Decision.py
+++ app/Decision.py
@@ -1,4 +1,42 @@
 import random, datetime
+def first_five():
+    try:
+        five_file = open('../resources/movefile.txt', 'w')
+        our_snake = board.our_snake
+        
+        valid_moves = { 'left': True,
+                        'right': True,
+                        'up': True,
+                        'down': True }
+
+        directions = ['left','right','up','down']
+
+        for direction in directions:
+            if our_snake.moves[direction] in board.get_bad_squares():
+                valid_moves[direction] = False
+
+        #Write turtle square moves to move file text
+        #Noodle will turtle up and go into a sqaure move pattern for
+        #the first few turns. Movement depends on which X and Y directions are clear
+        if valid_moves['up'] and valid_moves['right']:
+            five_file.write('right\ndown\nleft')
+            return 'up'
+        elif valid_moves['up'] and valid_moves['left']:
+            five_file.write('left\ndown\nright')
+            return 'up'
+        elif valid_moves['down'] and valid_moves['right']:
+            five_file.write('right\nup\nleft')
+            return 'down'
+        else:
+            five_file.write('left\nup\nright')
+            return 'down'
+
+    except IOError as e:
+        print(e)
+
+    finally:
+        five_file.close()
+
 def decide(board):
     our_snake = board.our_snake
     nearest_food = board.nearest_foods[our_snake.id]
@@ -73,41 +111,3 @@ def decide(board):
         'move': choice,
         'taunt': our_snake.id
     }
-
-def first_five():
-    try:
-        five_file = open('../resources/movefile.txt', 'w')
-        our_snake = board.our_snake
-        
-        valid_moves = { 'left': True,
-                        'right': True,
-                        'up': True,
-                        'down': True }
-
-        directions = ['left','right','up','down']
-
-        for direction in directions:
-            if our_snake.moves[direction] in board.get_bad_squares():
-                valid_moves[direction] = False
-
-        #Write turtle square moves to move file text
-        #Noodle will turtle up and go into a sqaure move pattern for
-        #the first few turns. Movement depends on which X and Y directions are clear
-        if valid_moves['up'] and valid_moves['right']:
-            five_file.write('right\ndown\nleft')
-            return 'up'
-        elif valid_moves['up'] and valid_moves['left']:
-            five_file.write('left\ndown\nright')
-            return 'up'
-        elif valid_moves['down'] and valid_moves['right']:
-            five_file.write('right\nup\nleft')
-            return 'down'
-        else:
-            five_file.write('left\nup\nright')
-            return 'down'
-
-    except IOError as e:
-        print(e)
-
-    finally:
-        five_file.close()
