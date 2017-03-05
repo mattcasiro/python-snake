57d8c973b3bda717b644a904c60db062510ba703
Matthew Casiro <mattcasiro@gmail.com>
2017-03-04 17:31:14 -0800

    Use array of taunts instead of taunt file

--- app/Taunt.py
+++ app/Taunt.py
@@ -8,16 +8,21 @@ from random import randint
 
 class Taunt:
     def populate_array(self):
-        taunt_file = open('taunts.txt', 'r')
-        for line in taunt_file:
-            self.taunt_array.append(line)
-        taunt_file.close()
+        return ["My food! Bork Bork Bork!",
+                "BORK! BORK! BORK! BORK!",
+                "Crush! Kill! Destory!",
+                "Life sure is ruff.:P",
+                "I am a chocolate brown caboose of doom!",
+                "Treats, yum yum yum!",
+                "I beat cancer, I'll beat you too.",
+                "GO FOR THE EYES!",
+                "I am the alpha!",
+                "Strudel rules!"]
 
     def grab_taunt(self):
         choice = randint(0,len(self.taunt_array) - 1)
         return self.taunt_array[choice]
 
     def __init__(self):
-        self.taunt_array = []
-        self.populate_array()
+        self.taunt_array = populate_array()
         
