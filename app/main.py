76692a36b46cbdb65f6d1f5bae821bf59172d680
Matthew Casiro <mattcasiro@gmail.com>
2018-09-23 14:18:01 -0700

    Update print statements to Python 3 syntax

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ def start():
 
     # TODO: Do things with data
 
-    print "Starting game %s" % data["game"]["id"]
+    print ("Starting game %s" % data["game"]["id"])
     return StartResponse("#00ff00")
 
 
@@ -37,9 +37,9 @@ def move():
     # TODO: Do things with data
     
     directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
+    direction = 'right'#random.choice(directions)
 
-    print "Moving %s" % direction
+    print ("Moving %s" % direction)
     return MoveResponse(direction)
 
 
@@ -49,7 +49,7 @@ def end():
 
     # TODO: Do things with data
 
-    print "Game %s ended" % data["game"]["id"]
+    print ("Game %s ended" % data["game"]["id"])
 
 
 # Expose WSGI app (so gunicorn can find it)
