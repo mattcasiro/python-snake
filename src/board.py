0f6ab49415bb914297339eed7a184e8d248526f6
JNotelddim <jaredmiddleton3.14@gmail.com>
2018-10-08 14:37:04 -0700

    Create board class

---
+++ src/board.py
@@ -0,0 +1,18 @@
+""" Board Module """
+
+class Board:
+    """ This class represents the board which tracks the cooardinates for all
+    snakes and food in a game"""
+    def __init__(self, data):
+        self._data = data
+        return
+
+    @property
+    def snake_list(self):
+        """snake_list retreives the list of snakes from the board data"""
+        return self._data.snakeList
+
+    @property
+    def food_list(self):
+        """food_list retreives the list of food from the board data"""
+        return self._data.foodList
