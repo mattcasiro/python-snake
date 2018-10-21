bfcc0901469412443b5eac64920ee85078289fcc
Jared Middleton <JNotelddim@users.noreply.github.com>
2018-10-20 20:57:44 -0700

    Create board class (#2)
    * Create board class
    * Create test
    * Use consistent formatting and docstrings based on PEP 8
    
    Use imperative in docstrings to better fit PEP8.
    For board list names, rather than '_list' suffixes use pluralized variable
    names for better intuitiveness.
    Use list indentation/formatting agreed upon for greatest clarity.

---
+++ src/board.py
@@ -0,0 +1,16 @@
+"""Board Module"""
+
+class Board:
+    """Track the cooardinates for all snakes and food in the game."""
+    def __init__(self, data):
+        self._data = data
+
+    @property
+    def snakes(self):
+        """Retreive the list of snakes from the board data."""
+        return self._data['snakes']
+
+    @property
+    def foods(self):
+        """Retreive the list of food from the board data."""
+        return self._data['food']
---
+++ test/board_test.py
@@ -0,0 +1,33 @@
+import pytest
+from src.board import Board
+
+class TestBoard:
+    def get_board(self):
+        snakes = [{
+            "coords": [{2, 2}, {2,3}, {3,3}],
+            "health": 10,
+            "id": "asdf1234"
+        }]
+        foods = [
+            {10, 5},
+            {6, 12}
+        ]
+        return Board({
+            "snakes" : snakes,
+            "food"  : foods
+        })
+
+    def test_create_board(self):
+        assert type(Board({})).__name__ == "Board"
+
+    def test_board_has_snakes(self):
+        assert self.get_board().snakes == [{
+            "coords": [{2, 2}, {2,3}, {3,3}],
+            "health": 10,
+            "id": "asdf1234"
+        }]
+    def test_board_has_foods(self):
+        assert self.get_board().foods == [
+            {10, 5},
+            {6, 12}
+        ]
