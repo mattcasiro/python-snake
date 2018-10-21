d1834637fd7b4add0c04afe67e1d925def4066e4
JNotelddim <jaredmiddleton3.14@gmail.com>
2018-10-20 20:07:58 -0700

    Update board property naming and docstrings
    Use imperative in docstrings to better fit PEP8.
    For board list names, replace '_list' suffixes with pluralized variable
    names for better intuitiveness.

--- src/board.py
+++ src/board.py
@@ -1,17 +1,16 @@
 """ Board Module """
 
 class Board:
-    """ This class represents the board which tracks the cooardinates for all
-    snakes and food in a game"""
+    """Track the cooardinates for all snakes and food in the game."""
     def __init__(self, data):
         self._data = data
 
     @property
-    def snake_list(self):
-        """snake_list retreives the list of snakes from the board data"""
+    def snakes(self):
+        """Retreive the list of snakes from the board data."""
         return self._data['snakes']
 
     @property
-    def food_list(self):
-        """food_list retreives the list of food from the board data"""
+    def foods(self):
+        """Retreive the list of food from the board data."""
         return self._data['food']
--- test/board_test.py
+++ test/board_test.py
@@ -3,34 +3,34 @@ from src.board import Board
 
 class TestBoard:
     def get_board(self):
-        snake_list = [
+        snakes = [
             {
                 "coords": [{2, 2}, {2,3}, {3,3}],
                 "health": 10,
                 "id": "asdf1234"
             }
         ]
-        food_list = [
+        foods = [
             {10, 5},
             {6, 12}
         ]
         return Board(
             {
-                "snakes" : snake_list,
-                "food"  : food_list
+                "snakes" : snakes,
+                "food"  : foods
             })
 
     def test_create_board(self):
         assert type(Board({})).__name__ == "Board"
 
     def test_board_has_snakes(self):
-        assert self.get_board().snake_list == [{
+        assert self.get_board().snakes == [{
             "coords": [{2, 2}, {2,3}, {3,3}],
             "health": 10,
             "id": "asdf1234"
         }]
-    def test_board_has_food(self):
-        assert self.get_board().food_list == [
+    def test_board_has_foods(self):
+        assert self.get_board().foods == [
             {10, 5},
             {6, 12}
         ]
