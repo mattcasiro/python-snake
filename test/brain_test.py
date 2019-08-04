1f6bb3183872712e4ee220563d6ce3bdfa47d13e
JNotelddim <jaredmiddleton3.14@gmail.com>
2019-08-04 13:59:44 -0700

    Test food paths allow escape
    (test not yet passing because the part in get_path which is supposed to
    be making sure the food is safe and offers a route back out isn't
    working)

--- test/brain_test.py
+++ test/brain_test.py
@@ -204,7 +204,7 @@ class TestBrain:
         ]
 
         brain = self.get_brain(a+b+c, foods, 11)
-        assert brain.get_decision() == "left"
+        assert brain.get_decision() ==  "left"
 
     def test_snake_avoids_self(self):
         a = copy.deepcopy(self.main_snake)
@@ -216,3 +216,92 @@ class TestBrain:
         brain = self.get_brain(a, [], 11)
         assert brain.get_decision() != "right"
 
+    def test_food_path_allows_escape(self):
+        foods = [
+            # Food:<> Food:<X:2 > Food:<X:5 Y:4 > Food:<X:12 > Food:<X:9 Y:2 > Food:<X:4 Y:5 > Food:<X:13 Y:13 > Food:<X:6 Y:6 > Food:<X:2 Y:6 >
+            (0,0), (1,0), (2,0), (12,0),
+            (9,2),
+            (5,4), (12,4),
+            (4,5),
+            (6,6),
+            (13,13),
+        ]
+
+        a = copy.deepcopy(self.main_snake)
+        a[0]["body"] = [
+        # Body:<X:12 Y:4 > Body:<X:13 Y:4 > Body:<X:12 Y:4 > Body:<X:11 Y:4 > Body:<X:11 Y:5 > Body:<X:12 Y:5 > Body:<X:13 Y:5 > Body:<X:13 Y:6 > Body:<X:12 Y:6 > Body:<X:11 Y:6 > Body:<X:11 Y:7 > Body:<X:12 Y:7 > Body:<X:12 Y:8 > Body:<X:11 Y:8 > Body:<X:11 Y:9 > Body:<X:11 Y:10 > Body:<X:10 Y:10 > Body:<X:9 Y:10 > Body:<X:9 Y:11 > Body:<X:9 Y:12 > Body:<X:8 Y:12 > Body:<X:7 Y:12 > Body:<X:6 Y:12 > Body:<X:5 Y:12 > Body:<X:5 Y:13 > Body:<X:4 Y:13 > Body:<X:3 Y:13 > Body:<X:2 Y:13 > Body:<X:2 Y:14 > Body:<X:3 Y:14 > Body:<X:4 Y:14 > Body:<X:5 Y:14 > Body:<X:6 Y:14 > Body:<X:6 Y:13 > Body:<X:7 Y:13 > Body:<X:8 Y:13 > Body:<X:9 Y:13 > Body:<X:10 Y:13 > Body:<X:10 Y:12 > Body:<X:10 Y:11 > Body:<X:11 Y:11 > Body:<X:12 Y:11 > Body:<X:12 Y:10 > Body:<X:12 Y:9 > Body:<X:13 Y:9 > Body:<X:13 Y:8 > Body:<X:14 Y:8 > Body:<X:14 Y:7 > Body:<X:14 Y:6 > Body:<X:14 Y:5 > Body:<X:14 Y:4 > Body:<X:14 Y:3 > Body:<X:14 Y:2 > Body:<X:13 Y:2 > Body:<X:13 Y:3 > Body:<X:12 Y:3 > Body:<X:11 Y:3 > Body:<X:10 Y:3 > Body:<X:10 Y:4 > Body:<X:10 Y:5 > Body:<X:10 Y:6 > Body:<X:10 Y:7 > Body:<X:10 Y:8 > Body:<X:9 Y:8 > Body:<X:8 Y:8 > Body:<X:7 Y:8 > Body:<X:6 Y:8 > Body:<X:5 Y:8 > Body:<X:4 Y:8 > Body:<X:3 Y:8 > 
+            # (12, 4),
+            # (13, 4),
+            # (12, 4),
+            # (11, 4),
+            # (11, 5),
+            # (12, 5),
+            # (13, 5),
+            # (13, 6),
+            # (12, 6),
+            # (11, 6),
+            # (11, 7),
+            # (12, 7),
+            # (12, 8),
+            # (11, 8),
+            (11, 9),
+            (11, 10),
+            (10, 10),
+            (9, 10),
+            (9, 11),
+            (9, 12),
+            (8, 12),
+            (7, 12),
+            (6, 12),
+            (5, 12),
+            (5, 13),
+            (4, 13),
+            (3, 13),
+            (2, 13),
+            (2, 14),
+            (3, 14),
+            (4, 14),
+            (5, 14),
+            (6, 14),
+            (6, 13),
+            (7, 13),
+            (8, 13),
+            (9, 13),
+            (10, 13),
+            (10, 12),
+            (10, 11),
+            (11, 11),
+            (12, 11),
+            (12, 10),
+            (12, 9),
+            (13, 9),
+            (13, 8),
+            (14, 8),
+            (14, 7),
+            (14, 6),
+            (14, 5),
+            (14, 4),
+            (14, 3),
+            (14, 2),
+            (13, 2),
+            (13, 3),
+            (12, 3),
+            (11, 3),
+            (10, 3),
+            (10, 4),
+            (10, 5),
+            (10, 6),
+            (10, 7),
+            (10, 8),
+            (9, 8),
+            (8, 8),
+            (7, 8),
+            (6, 8),
+            (5, 8),
+            (4, 8),
+            (3, 8),
+            (2,8), (2, 7), (1,7), (1,8), (0,8), (0,9), (0,10), (0,11), (1,11), (1,10), (2,10), (3,10), (4,10)
+        ]
+
+        brain = self.get_brain(a, foods, 15)
+        assert brain.get_decision() != "up"
